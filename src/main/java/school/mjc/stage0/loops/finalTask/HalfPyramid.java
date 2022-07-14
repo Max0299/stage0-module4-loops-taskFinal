5c9d8eb4adcaf0e19a1e76aab160f2e00a6c42f7
Max Ageev <ageev.makson@yandex.ru>
2022-07-14 17:07:49 +0300

    commit

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,14 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+     for(int i = 1;i<=cathetusLength;i++){
+         for(int j = 1;j<=cathetusLength;j++){
+             if(i <=j){
+                 System.out.println("*");
+             }
+         }
+         System.out.println();
+     }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+      for(int i = 0;i<=cathetusLength;i++){
+          for(int j = 0;j<=cathetusLength;j++){
 
+          }
+      }
     }
 
     public static void main(String[] args) {
